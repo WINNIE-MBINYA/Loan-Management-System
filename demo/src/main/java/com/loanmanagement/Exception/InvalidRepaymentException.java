66882832d19bc806cf34f11ee0da9065d1ea5b53
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-02-27 08:42:05 +0300

    Refactor InvalidRepaymentException for clearer error handling

--- demo/src/main/java/com/loanmanagement/Exception/InvalidRepaymentException.java
+++ demo/src/main/java/com/loanmanagement/Exception/InvalidRepaymentException.java
@@ -1,4 +1,7 @@
 package com.loanmanagement.Exception;
 
-public class InvalidRepaymentException {
+public class InvalidRepaymentException extends RuntimeException {
+    public InvalidRepaymentException(String message) {
+        super(message);
+    }
 }
