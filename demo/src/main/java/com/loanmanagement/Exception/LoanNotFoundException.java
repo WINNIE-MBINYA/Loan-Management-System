e9ea89af6a32d2c2f25c2ccd437d856bf9095664
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-02-27 08:42:19 +0300

    Improve LoanNotFoundException message clarity

--- demo/src/main/java/com/loanmanagement/Exception/LoanNotFoundException.java
+++ demo/src/main/java/com/loanmanagement/Exception/LoanNotFoundException.java
@@ -1,4 +1,7 @@
 package com.loanmanagement.Exception;
 
-public class LoanNotFoundException {
+public class LoanNotFoundException extends RuntimeException {
+    public LoanNotFoundException(String message) {
+        super(message);
+    }
 }
