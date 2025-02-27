3ee9b55ca95d42a43d071a02b144471c54673c6a
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-02-27 08:40:51 +0300

    Update LoanStatus enum to ensure consistent loan lifecycle states

--- demo/src/main/java/com/loanmanagement/Entity/LoanStatus.java
+++ demo/src/main/java/com/loanmanagement/Entity/LoanStatus.java
@@ -1,4 +1,10 @@
 package com.loanmanagement.Entity;
 
-public class LoanStatus {
+public enum LoanStatus {
+    PENDING,    // Loan application submitted, awaiting review
+    APPROVED,   // Loan has been approved, awaiting disbursement
+    ACTIVE,     // Loan is disbursed and being repaid
+    PAID,       // Loan has been fully repaid
+    DEFAULTED,  // Loan payments were missed and the loan defaulted
+    REJECTED    // Loan application was denied
 }
