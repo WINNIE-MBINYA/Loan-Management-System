1a8c177bd78483efa8c98cb78b4829928cee942a
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-02-27 08:40:00 +0300

    Implement loan statistics retrieval in StatisticsController

--- demo/src/main/java/com/loanmanagement/Controller/StatisticsController.java
+++ demo/src/main/java/com/loanmanagement/Controller/StatisticsController.java
@@ -1,4 +1,23 @@
 package com.loanmanagement.Controller;
 
+import com.loanmanagement.Dto.StatisticsDTO;
+import com.loanmanagement.Service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/statistics")
+@RequiredArgsConstructor
 public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/summary")
+    public ResponseEntity<StatisticsDTO> getStatisticsSummary() {
+        StatisticsDTO statistics = statisticsService.getLoanStatistics();
+        return ResponseEntity.ok(statistics);
+    }
 }
