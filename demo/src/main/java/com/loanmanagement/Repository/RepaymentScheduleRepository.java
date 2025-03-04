5c734e8efad5caf8c6852a37bccf7a614faca925
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-03-04 16:49:28 +0300

    Optimize RepaymentScheduleRepository query methods

--- demo/src/main/java/com/loanmanagement/Repository/RepaymentScheduleRepository.java
+++ demo/src/main/java/com/loanmanagement/Repository/RepaymentScheduleRepository.java
@@ -7,12 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.time.LocalDate;
 
 @Repository
 public interface RepaymentScheduleRepository extends JpaRepository<RepaymentSchedule, Long> {
 
     // Get the repayment schedule for a specific loan
     List<RepaymentSchedule> findByLoan(Loan loan);
+
+    List<RepaymentSchedule> findByLoanId(Long loanId);
+
+    List<RepaymentSchedule> findByLoanIdAndDueDateAfterAndStatus(Long loanId, LocalDate today, String status);
 
     List<RepaymentSchedule> findByLoanAndStatus(Loan loan, PaymentStatus paymentStatus);
 }
