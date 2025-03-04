8d9fe75bcd29e8640b304b0e617a992149265f92
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2025-03-04 15:19:27 +0300

    Modify LoanIssuanceDTO for better data handling

--- demo/src/main/java/com/loanmanagement/Dto/LoanIssuanceDTO.java
+++ demo/src/main/java/com/loanmanagement/Dto/LoanIssuanceDTO.java
@@ -1,7 +1,9 @@
 package com.loanmanagement.Dto;
 
 import com.loanmanagement.Entity.RepaymentFrequency;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -9,11 +11,12 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor  // Ensures there's a no-args constructor for frameworks like Jackson
+@AllArgsConstructor // Adds a constructor that takes all fields as parameters
 public class LoanIssuanceDTO {
-    private Long customerId;
     private BigDecimal principalAmount;
     private BigDecimal interestRate;
     private int repaymentPeriod;
     private RepaymentFrequency repaymentFrequency;
-    private LocalDate loanIssuedDate;;
+    private LocalDate loanIssuedDate;
 }
